c99d2edac09d760a98202474e25b54869987c211
dereck-ruuiz <dcabrerar2300@alumno.ipn.mx>
2025-10-11 00:02:34 -0600

    Se termina el ejercicio 4 de la práctica 05

--- II/05 Ejercicios/Ejercicio 4/javascript/funcionese4.js
+++ II/05 Ejercicios/Ejercicio 4/javascript/funcionese4.js
@@ -19,41 +19,35 @@ function calcularc(){
     var parse2 = parseFloat(cal2);
     var parse3 = parseFloat(cal3);
     var parseef = parseFloat(caleef);
-    var parstf = parseFloat(caltf);
+    var parsetf = parseFloat(caltf);
 
     if(isNaN(parse1) || isNaN(parse2) || isNaN(parse3) || isNaN(parseef) || isNaN(parsetf)){
         alert("Por favor, rellena todos los campos.");
+        return;
+    }
+
+    if(parse1 > 10 || parse2 > 10 || parse3 > 10 || parseef > 10 || parsetf > 10){
+        alert("Por favor, ingresa una calificación válida (no mayor a diez).");
+        return;
+    }
+
+    var calif = (parse1 + parse2 + parse3)/3
+    var calift = calif*0.55;
+    var califte = parseef*0.3;
+    var califttf = parsetf*0.15;
+
+    var calificacion_total = calift + califte + califttf;
+    
+    document.getElementById("calificacionf").value =
+    "Su calificación final: " + calificacion_total.toFixed(2);
+}
+
+function borrarc(){
         document.getElementById("calificacion1").value = "";
         document.getElementById("calificacion2").value = "";
         document.getElementById("calificacion3").value = "";
         document.getElementById("calificacione").value = "";
         document.getElementById("calificaciontf").value = "";
-        return;
-    }
-
-    if(isNaN() || isNaN() || isNaN()){
-        alert("Rellene todos los campos (introduce cero si no hubo ventas).");
-        return;
-    }
-
-    var  = *0.1;
-    var  = *0.1;
-    var  = *0.1;
-
-    var   =  + ;
-    
-    document.getElementById("sueldot").value =
-    "Su primera comisión: $ " + com1.toFixed(2) +
-    "\nSu segunda comisión: $ " + com2.toFixed(2) +
-    "\nSu tercera comisión: $ " + com3.toFixed(2) +
-    "\nSu sueldo neto: $ " + sueldo_total.toFixed(2);
-}
-
-function borrarc(){
-        document.getElementById("sueldob").value = "";
-        document.getElementById("venta1").value = "";
-        document.getElementById("venta2").value = "";
-        document.getElementById("venta3").value = "";
-        document.getElementById("sueldot").value = "";
+        document.getElementById("calificacionf").value = "";
 }
 
