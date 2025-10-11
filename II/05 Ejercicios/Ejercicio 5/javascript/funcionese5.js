a635440c713f282b2ad05b9255084918f5007162
dereck-ruuiz <dcabrerar2300@alumno.ipn.mx>
2025-10-11 17:05:23 -0600

    Se concluye el ejercicio 5 de la práctica 05

---
+++ II/05 Ejercicios/Ejercicio 5/javascript/funcionese5.js
@@ -0,0 +1,36 @@
+function validarn(e){
+    var teclado = (document.all)? e.keyCode : e.which;
+    if(teclado == 8) return true;
+    var patron = /[0-9\d .]/;
+
+    var codigo =  String.fromCharCode(teclado);
+    return patron.test(codigo);
+}
+
+function calcularAlumnos(){
+    var men = document.getElementById("hombres").value;
+    var wmen = document.getElementById("mujeres").value;
+
+    var parse_men = parseInt(men)
+    var parse_wmen = parseInt(wmen)
+
+    if(isNaN(parse_men) || isNaN(parse_wmen)){
+        alert("rellene todos los campos");
+        return;
+    }
+    
+    var total = parse_men + parse_wmen
+    var prc_men = parse_men*100/total
+    var prc_wmen = parse_wmen*100/total
+    
+    document.getElementById("porcentajesa").value =
+    "Cantidad de mujeres: " + prc_wmen.toFixed(2) + "%" +
+    "\nCantidad de hombres: " + prc_men.toFixed(2) + "%";
+}
+
+function borrarc(){
+        document.getElementById("hombres").value = "";
+        document.getElementById("mujeres").value = "";
+        document.getElementById("porcentajesa").value = "";
+}
+
