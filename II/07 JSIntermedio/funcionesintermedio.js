60cf2a2afa96672b3f9fb7e6cafc4d9eb36341b1
dereck-ruuiz <dcabrerar2300@alumno.ipn.mx>
2025-10-13 20:28:44 -0600

    clase del 13/10/25

---
+++ II/07 JSIntermedio/funcionesintermedio.js
@@ -0,0 +1,12 @@
+const razasDePerros =  [
+    "Pastor Aleman",
+    "Labrador Retriever",
+    "Bulldog Francés",
+    "Beagle",
+    "Chihuahua",
+    "Dalmata",
+    "Salchicha",
+    "Pug",
+]
+
+razasDePerros.forEach(raza => console.log(raza));
